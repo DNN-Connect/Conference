1e6eda30afec7dbbcc29a1615fdbf8ded8e09f45
Peter Donker <peter@bring2mind.net>
2015-12-22 19:48:41 +0100

    Added one more level with tracks and editing/adding tracks

--- Controllers/ConferenceController.cs
+++ Controllers/ConferenceController.cs
@@ -20,6 +20,16 @@ namespace Connect.DNN.Modules.Conference.Controllers
         }
 
         [HttpGet]
+        public ActionResult View(int conferenceId)
+        {
+            var conference = _repository.GetConference(PortalSettings.PortalId, conferenceId);
+            if (conference == null) { conference = new Connect.Conference.Core.Models.Conferences.Conference() { PortalId = PortalSettings.PortalId }; }
+            AddBootstrap();
+            AddModuleScript();
+            return View(conference);
+        }
+
+        [HttpGet]
         public ActionResult Edit(int conferenceId)
         {
             var conference = _repository.GetConference(PortalSettings.PortalId, conferenceId);
---
+++ Controllers/TrackController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using DotNetNuke.Common;
+using Connect.DNN.Modules.Conference.Common;
+using Connect.Conference.Core.Repositories;
+using Connect.Conference.Core.Models.Tracks;
+
+namespace Connect.DNN.Modules.Conference.Controllers
+{
+    public class TrackController : ConferenceMvcController
+    {
+        private readonly ITrackRepository _repository;
+
+        public TrackController() : this(TrackRepository.Instance) { }
+
+        public TrackController(ITrackRepository repository)
+        {
+            Requires.NotNull(repository);
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult View(int conferenceId, int trackId)
+        {
+            var track = _repository.GetTrack(conferenceId, trackId);
+            if (track == null) { track = new Track() { ConferenceId = conferenceId }; }
+            AddBootstrap();
+            AddModuleScript();
+            return View(track);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int conferenceId, int trackId)
+        {
+            var track = _repository.GetTrack(conferenceId, trackId);
+            if (track == null) { track = new Track() { ConferenceId = conferenceId }; }
+            AddBootstrap();
+            AddModuleScript();
+            AddEditScripts();
+            return View(track.GetTrackBase());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TrackBase track)
+        {
+            var previousRecord = _repository.GetTrack(track.ConferenceId, track.TrackId);
+            if (previousRecord == null)
+            {
+                _repository.AddTrack(track, User.UserID);
+            }
+            else
+            {
+                track.CreatedOnDate = previousRecord.CreatedOnDate;
+                track.CreatedByUserID = previousRecord.CreatedByUserID;
+                _repository.UpdateTrack(track, User.UserID);
+            }
+            return View("View", _repository.GetTrack(track.ConferenceId, track.TrackId));
+        }
+
+    }
+}
