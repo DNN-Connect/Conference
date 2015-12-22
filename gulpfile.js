8d26ef33d2c33ab5c38309e7fa6549c134635abf
Peter Donker <peter@bring2mind.net>
2015-12-22 12:07:57 +0100

    Setting up the gulp build system differently to compile css and js to their folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
 var gulp = require('gulp'),
- path = require('path'),
- less = require('gulp-less');
+  path = require('path'),
+  concat = require('gulp-concat'),
+  less = require('gulp-less'),
+  browserify = require('browserify');
 
-gulp.task('less', function () {
-  return gulp.src('src/module.less')
+gulp.task('less', function() {
+  return gulp.src('src/less/module.less')
     .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+      paths: [path.join(__dirname, 'less', 'includes')]
     }))
+    .pipe(gulp.dest('src'));
+});
+
+gulp.task('css', ['less'], function() {
+  return gulp.src(['src/module.css',
+      'bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css'
+    ])
+    .pipe(concat('module.css'))
     .pipe(gulp.dest('.'));
 });
+
+gulp.task('js', function() {
+  return gulp.src([
+    'bower_components/moment/min/moment.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
+    ])
+  .pipe(gulp.dest('js'));
+});
+
