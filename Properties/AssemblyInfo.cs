8c497c05f78130209930eccab6033ca01812b828
Peter Donker <peter@bring2mind.net>
2018-05-16 16:38:22 +0200

    Release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.12")]
-[assembly: AssemblyFileVersion("2.1.12")]
+[assembly: AssemblyVersion("2.1.13")]
+[assembly: AssemblyFileVersion("2.1.13")]
 [assembly: AssemblyInformationalVersion("01.00.00")]
